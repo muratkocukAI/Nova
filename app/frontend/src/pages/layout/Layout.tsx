2d6384c619de09c25a260091e4b2d2160e2e9738
Murat Kocuk <131297520+muratkocukAI@users.noreply.github.com>
2024-01-16 23:02:11 +0300

    layout-test

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,55 +1,97 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
-
 import github from "../../assets/github.svg";
-
 import styles from "./Layout.module.css";
-
 import { useLogin } from "../../authConfig";
-
 import { LoginButton } from "../../components/LoginButton";
 
 const Layout = () => {
-    return (
-        <div className={styles.layout}>
-            <header className={styles.header} role={"banner"}>
-                <div className={styles.headerContainer}>
-                    <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
-                    </Link>
-                    <nav>
-                        <ul className={styles.headerNavList}>
-                            <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
-                                    <img
-                                        src={github}
-                                        alt="Github logo"
-                                        aria-label="Link to github repository"
-                                        width="20px"
-                                        height="20px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
-                    {useLogin && <LoginButton />}
-                </div>
-            </header>
-
-            <Outlet />
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header} role={"banner"}>
+        <div className={styles.headerContainer}>
+          <Link to="/" className={styles.headerTitleContainer}>
+            <h3 className={styles.headerTitle}>
+              Nova AI | Business Intelligence Redefined
+            </h3>
+          </Link>
+          <nav>
+            <ul className={styles.headerNavList}>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? styles.headerNavPageLinkActive
+                      : styles.headerNavPageLink
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className={styles.headerNavLeftMargin}>
+                <NavLink
+                  to="/explore"
+                  className={({ isActive }) =>
+                    isActive
+                      ? styles.headerNavPageLinkActive
+                      : styles.headerNavPageLink
+                  }
+                >
+                  Explore Data
+                </NavLink>
+              </li>
+              <li className={styles.headerNavLeftMargin}>
+                <NavLink
+                  to="/insights"
+                  className={({ isActive }) =>
+                    isActive
+                      ? styles.headerNavPageLinkActive
+                      : styles.headerNavPageLink
+                  }
+                >
+                  Insights
+                </NavLink>
+              </li>
+              <li className={styles.headerNavLeftMargin}>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive
+                      ? styles.headerNavPageLinkActive
+                      : styles.headerNavPageLink
+                  }
+                >
+                  About Nova AI
+                </NavLink>
+              </li>
+              <li className={styles.headerNavLeftMargin}>
+                <a
+                  href="https://ai.crimsoninnovate.com"
+                  target={"_blank"}
+                  title="Nova AI by Crimson Consulting"
+                >
+                  <img
+                    src={github}
+                    alt="Crimson Consulting"
+                    aria-label="Link to Nova AI github repository"
+                    width="20px"
+                    height="20px"
+                    className={styles.githubLogo}
+                  />
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <h4 className={styles.headerRightText}>
+            Empowering Intelligence with Azure and AI
+          </h4>
+          {useLogin && <LoginButton />}
         </div>
-    );
+      </header>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
