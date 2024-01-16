7154abce70f11a479b6831cf0bbf6cef873c9c0a
Murat Kocuk <131297520+muratkocukAI@users.noreply.github.com>
2024-01-16 23:06:42 +0300

    Update Layout.tsx

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -3,95 +3,58 @@ import github from "../../assets/github.svg";
 import styles from "./Layout.module.css";
 import { useLogin } from "../../authConfig";
 import { LoginButton } from "../../components/LoginButton";
-
 const Layout = () => {
-  return (
-    <div className={styles.layout}>
-      <header className={styles.header} role={"banner"}>
-        <div className={styles.headerContainer}>
-          <Link to="/" className={styles.headerTitleContainer}>
-            <h3 className={styles.headerTitle}>
-              Nova AI | Business Intelligence Redefined
-            </h3>
-          </Link>
-          <nav>
-            <ul className={styles.headerNavList}>
-              <li>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive
-                      ? styles.headerNavPageLinkActive
-                      : styles.headerNavPageLink
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className={styles.headerNavLeftMargin}>
-                <NavLink
-                  to="/explore"
-                  className={({ isActive }) =>
-                    isActive
-                      ? styles.headerNavPageLinkActive
-                      : styles.headerNavPageLink
-                  }
-                >
-                  Explore Data
-                </NavLink>
-              </li>
-              <li className={styles.headerNavLeftMargin}>
-                <NavLink
-                  to="/insights"
-                  className={({ isActive }) =>
-                    isActive
-                      ? styles.headerNavPageLinkActive
-                      : styles.headerNavPageLink
-                  }
-                >
-                  Insights
-                </NavLink>
-              </li>
-              <li className={styles.headerNavLeftMargin}>
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) =>
-                    isActive
-                      ? styles.headerNavPageLinkActive
-                      : styles.headerNavPageLink
-                  }
-                >
-                  About Nova AI
-                </NavLink>
-              </li>
-              <li className={styles.headerNavLeftMargin}>
-                <a
-                  href="https://ai.crimsoninnovate.com"
-                  target={"_blank"}
-                  title="Nova AI by Crimson Consulting"
-                >
-                  <img
-                    src={github}
-                    alt="Crimson Consulting"
-                    aria-label="Link to Nova AI github repository"
-                    width="20px"
-                    height="20px"
-                    className={styles.githubLogo}
-                  />
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <h4 className={styles.headerRightText}>
-            Empowering Intelligence with Azure and AI
-          </h4>
-          {useLogin && <LoginButton />}
-        </div>
-      </header>
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>Nova AI | Business Intelligence Redefined</h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/explore" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Explore Data
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/insights" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Insights
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/about" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    About Nova AI
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <a href="https://ai.crimsoninnovate.com" target={"_blank"} title="Nova AI by Crimson Consulting">
+                                    <img
+                                        src={github}
+                                        alt="Crimson Consulting"
+                                        aria-label="Link to Nova AI github repository"
+                                        width="20px"
+                                        height="20px"
+                                        className={styles.githubLogo}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h4 className={styles.headerRightText}>Empowering Intelligence with Azure and AI</h4>
+                    {useLogin && <LoginButton />}
+                </div>
+            </header>
 
-      <Outlet />
-    </div>
-  );
+            <Outlet />
+        </div>
+    );
 };
 
 export default Layout;
