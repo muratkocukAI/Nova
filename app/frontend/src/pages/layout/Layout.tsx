a9bb3f0cd1210e9626b739f4f9629b6e088db8dc
Murat Kocuk <131297520+muratkocukAI@users.noreply.github.com>
2024-01-17 17:21:24 +0000

    NovaCSSUpdatev1

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,44 +1,36 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
-
 import github from "../../assets/github.svg";
-
 import styles from "./Layout.module.css";
-
 import { useLogin } from "../../authConfig";
-
 import { LoginButton } from "../../components/LoginButton";
 
 const Layout = () => {
     return (
         <div className={styles.layout}>
-            <header className={styles.header} role={"banner"}>
+            <header className={styles.header} role="banner">
                 <div className={styles.headerContainer}>
-                    <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>Nova AI: Reinventing Business Intelligence with Advanced AI</h3>
+                    <Link to="/" className={styles.headerTitleContainer} aria-label="Home">
+                        <h1 className={styles.headerTitle}>Nova AI: BI with Advanced AI | Crimson Consulting</h1>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
                             <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}
+                                    aria-label="Talk to AI"
+                                >
+                                    Talk to AI
                                 </NavLink>
                             </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
+                            <li>
+                                <NavLink
+                                    to="/qa"
+                                    className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}
+                                    aria-label="Inquire AI"
+                                >
+                                    Inquire AI
                                 </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
-                                    <img
-                                        src={github}
-                                        alt="Github logo"
-                                        aria-label="Link to github repository"
-                                        width="20px"
-                                        height="20px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
                             </li>
                         </ul>
                     </nav>
